6488281dd06c29531137d5fdb21d4c505ca43d36
Jeanette <khojeanette@gmail.com>
2024-01-26 13:48:59 +0000

    Finished Exercise 9 Variables, Part 1. Updated the jen_types_practice file

--- jen_types_practice.py
+++ jen_types_practice.py
@@ -1,1 +1,19 @@
-# Testing 1 2 3
+first_name = 'Jeanette'
+last_name = 'Kho'
+# Created two variables, first_name and last_name, with string values using snake case
+
+print(first_name + ' ' + last_name)
+# The print function concatenates the string values of first_name, a space and last_name
+
+full_name_list = [first_name, last_name]
+# Created a list, full_name_list, with the values of first_name and last_name as its items
+
+print(full_name_list)
+# Print function outputs the items in the full_name_list
+
+full_name_dict = {'first': first_name, 'last': last_name}
+# Created a dictionary, full_name_dict, with the keys 'first' and 'last'
+# Used first_name and last_name variables as values for the keys
+
+print(full_name_dict)
+# Print function outputs the keys and values in the dictionary, full_name_dict
