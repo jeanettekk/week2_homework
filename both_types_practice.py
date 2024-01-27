37200b2b4f31e848a97401dcc61daf6ff7d9a61c
Anagha <mailsforanagha@gmail.com>
2024-01-27 18:57:08 +0000

    Committing both_types_practice.py

---
+++ both_types_practice.py
@@ -0,0 +1,30 @@
+# week 2 homework part 1
+
+# Created two variables, first_name and last_name, with string values using snake case
+first_name = "Anagha"
+last_name = "Jawalekar"
+
+# Following statement prints the values of variables assigned above
+# prints in new line using the new line character '\n'
+print("\n", first_name, last_name)
+
+# Following statement assigns list values to variable full_name
+full_name = [first_name, last_name]
+
+# Following statement prints the two values in the list
+print("\n", full_name[0], full_name[1])
+
+first_name = "Jeanette"
+last_name = "Kho"
+
+# The print function concatenates the string values of first_name, a space and last_name
+print('\n', first_name + ' ' + last_name)
+
+# Created a dictionary, full_name_dict, with the keys 'first' and 'last'
+# Used first_name and last_name variables as values for the keys
+full_name_dict = {'first': first_name, 'last': last_name}
+
+# Print function outputs the keys and values in the dictionary, full_name_dict
+print('\n', full_name_dict)
+
+
